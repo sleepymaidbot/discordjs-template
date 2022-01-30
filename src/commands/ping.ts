86386766330d711185e52f5995b5c626242556da
Antoine <antoine.turgeon2005@hotmail.com>
2022-01-29 21:37:28 -0500

    First Commit

---
+++ src/commands/ping.ts
@@ -0,0 +1,12 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+
+module.exports = {
+    guildIds: ["821717486217986098"],
+    data: new SlashCommandBuilder()
+        .setName('ping')
+        .setDescription('Pong!').toJSON(),
+
+    async exec(interaction) {
+        interaction.reply('Pong!')
+    }
+}
